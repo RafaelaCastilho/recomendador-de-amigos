4567275ee92f093c13c796390275d5049326b8a1
RafaelaCastilho <rafaela.c.pereira09@gmail.com>
2023-08-11 11:47:39 -0300

    Testes automatizados TDD

--- tests/getRecommendation.test.js
+++ tests/getRecommendation.test.js
@@ -1,23 +1,91 @@
-const controller = require('../src/controllers/controller');
-const personData = require('../src/data/mockData').person_tb;
-const relationData = require('../src/data/mockData').relationship_tb;
+const request = require('supertest');
+const app = require('../app');
 
-describe('getRecommendation', () => {
-    test('cpf com menos de 11 dígitos ou com caracter deve retornar erro 400', () => {
-        const req = {
-            params: {
-                cpf: '12345678' 
-            }
-        };
-        const res = {
-            status: jest.fn().mockReturnThis(),
-            json: jest.fn()
-        };
-        controller.getRecommendations(req, res);
-        expect(res.status).toHaveBeenCalledWith(200);
+const router = require('../src/routes/index');
 
+describe('Person API', ()=> {
+    describe('Busca de person', () => {
+        test('Se o CPF estiver cadastrado retorna 200', async () => {
+            const response = await request(app).get('/person/12345678900');
+            expect(response.status).toBe(200);
+        });
+    
+        test('Se o CPF não estiver cadastrado retorna 404', async () => {
+            const response = await request(app).get('/person/1234567890');
+            expect(response.status).toBe(404);
+        });
+    
+    });
+    
+    describe('Cadastro de person', () => {
+        test('Se o CPF conter 11 dígitos numéricos e não estiver cadastrado retorna 200', async () => {
+            const response = await request(app).post('/person').send({ "cpf": `${12346578900}`, "name": "Liv linda" });
+            expect(response.status).toBe(200);
+        });
+    
+        test('Se o CPF for diferente de 11 dígitos retorna 400', async () => {
+            const response = await request(app).post('/person').send({ "cpf": `${1234657890}`, "name": "Liv linda" });
+            expect(response.status).toBe(400);
+        });
+    
+        test('Se o CPF conter algum caractere retorna 400', async () => {
+            const response = await request(app).post('/person').send({ "cpf": "123457891a", "name": "Liv linda" });
+            expect(response.status).toBe(400);
+        });
+    
+        test('Se o CPF já estiver cadastrado retorna 400', async () => {
+            const response = await request(app).post('/person').send({ "cpf": `${12345678900}`, "name": "Liv linda" });
+            expect(response.status).toBe(400);
+        });
+    });
+});
+
+describe('Cadastro de relationship', () => {
+
+    test('Se os dois usuários existirem e ainda não houver um relacionamento retorna 200', async () => {
+        const response = await request(app).post('/relationship').send({ "cpf1": "12340000000", "cpf2": "12345678000" });
+        expect(response.status).toBe(200);
     });
 
+    test('Se o um dos usuários não existir retorna 404', async () => {
+        const response = await request(app).post('/relationship').send({ "cpf1": "1234567890", "cpf2": "12345678000" });
+        expect(response.status).toBe(404);
+    });
 
-    
-})
+    test('Se o relacionamento já existir retorna 400', async () => {
+        const response = await request(app).post('/relationship').send({ "cpf1": "12345678900", "cpf2": "12345678000" });
+        expect(response.status).toBe(400);
+    });
+
+});
+
+describe('Recomendação de amigos', () => {
+    test('Se o CPF estiver cadastrado retorna 200', async () => {
+        const response = await request(app).get('/recommendations/12345678900');
+        expect(response.status).toBe(200);
+    });
+
+    test('Se o CPF for diferente de 11 dígitos retorna 400', async () => {
+        const response = await request(app).get('/recommendations/12345678');
+        expect(response.status).toBe(400);
+    });
+
+    test('Se o CPF conter algum caractere retorna 400', async () => {
+        const response = await request(app).get('/recommendations/123456791a');
+        expect(response.status).toBe(400);
+    });
+
+    test('Se o CPF não estiver cadastrado retorna 404', async () => {
+        const response = await request(app).get('/recommendations/12345678999');
+        expect(response.status).toBe(404);
+    });
+
+});
+
+describe('Exclusão de dados', () => {
+    test('Deve excluir todos os registros', async () => {
+        const response = await request(app).delete('/clean');
+        expect(response.status).toBe(200);
+    });
+
+});
